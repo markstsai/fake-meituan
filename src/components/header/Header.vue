bfebc94b3a63ead22a3ce11c9963b0643cf0c92d
MarksTsai <p101424@hotmail.com>
2021-10-18 23:45:03 +0800

    Header.vue top-wrapper部分結構與CSS

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,6 +1,28 @@
 <template>
     <div class="header">
-        頭部
+
+        <!-- 頂部通欄 -->
+        <div class="top-wrapper">
+            <div class="back-wrapper">
+                <span class="icon-arrow_lift"></span>
+            </div>
+
+            <form class="search-wrapper">
+                <span class="search-icon"></span>
+                <input class="search-bar" type="text" placeholder="搜索店內商品">
+            </form>
+
+            <div class="more-wrapper">
+                <a class="spelling-bt" href="#"></a>
+                <div class="more-bt"></div>
+            </div>
+        </div>
+
+        <!-- 主體內容 -->
+        <div class="content-wrapper"></div>
+        
+        <!-- 公告內容 -->
+        <div class="bulletin-wrapper"></div>
     </div>
 </template>
 
@@ -9,4 +31,8 @@ export default {};
 </script>
 
 <style>
+/*導入字體圖標*/
+@import url("../../common/sytle/icon.css");
+/* 導入CSS*/
+@import url("Header.css");
 </style>
